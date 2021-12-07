9c7c2a975fa3d8f9b7fe95009ad45605b53b0b3c
Luiza Barros <88062746+luizabarros@users.noreply.github.com>
2021-12-07 13:38:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 mostrarCookies('cookies')
 function mostrarCookies(pegar_classe) {
     var pop_up = document.getElementsByClassName(pegar_classe)[0]
-    var aceitar_cookies = document.getElementsByClassName('aceitar')[0]
+        aceitar_cookies = document.getElementsByClassName('aceitar')[0]
     
     if (localStorage.botaoAceitar != aceitar_cookies) {
         pop_up.classList.add('show')
@@ -9,7 +9,7 @@ function mostrarCookies(pegar_classe) {
         aceitar_cookies.addEventListener('click', fecharCookies)
         function fecharCookies() {
             pop_up.classList.remove('show')
-            var localStorage.botaoAceitar = aceitar_cookies
+            localStorage.botaoAceitar = aceitar_cookies
         }
     }
 }
