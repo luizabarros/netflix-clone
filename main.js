4e86728bbfdf0ab392cd4d8acf878676bf57a537
Ana da Silva <thecoderana@gmail.com>
2022-03-07 09:31:14 -0300

    added var

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 mostrarCookies('cookies')
 function mostrarCookies(pegar_classe) {
     var pop_up = document.getElementsByClassName(pegar_classe)[0]
-        aceitar_cookies = document.getElementsByClassName('aceitar')[0]
+    var aceitar_cookies = document.getElementsByClassName('aceitar')[0]
     
     if (localStorage.botaoAceitar != aceitar_cookies) {
         pop_up.classList.add('show')
